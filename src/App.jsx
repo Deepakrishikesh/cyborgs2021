4a30350e362e8aeb536136cb3ce7c7e005034c32
Solai Raj <msraj085@gmail.com>
2021-07-30 11:43:16 +0530

    tailwind css intergration

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Landing from './pages/Landing';
+
+function App() {
+  return (
+    <>
+      <main>
+        <section>
+          <Landing/>
+        </section>
+        <section>
+
+        </section>
+        <section>
+
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,19 @@
+function Landing() {
+    return(
+        <>
+            <div className="">
+                <h1>
+                    CyBORGS
+                </h1>
+                <p>
+                    some content here ,
+                    some content here ,
+                    some content here ,
+                    some content here ,
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default Landing;
