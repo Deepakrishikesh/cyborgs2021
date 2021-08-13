dab3733694b0918745cd52e04d65bcb335b35d7f
Solai Raj <msraj085@gmail.com>
2021-08-13 09:10:05 +0530

    added some animations on ppl cards

--- src/pages/People.jsx
+++ src/pages/People.jsx
@@ -4,12 +4,12 @@ import iu from "../assets/event_logos/iu.jpg"
 const People = () => {
   return (
     <div className="lg:flex lg:flex-wrap justify-center">
-      <PeopleCard name="Miss S. Sneha" desig="President" phone="6381484247" photo={iu} />
-      <PeopleCard name="Miss Someone" desig="Designation" phone="1234567890" photo={iu}/>
-      <PeopleCard name="Miss Someone" desig="Designation" phone="1234567890" photo={iu}/>
-      <PeopleCard name="Miss Someone" desig="Designation" phone="1234567890" photo={iu}/>
-      <PeopleCard name="Miss Someone" desig="Designation" phone="1234567890" photo={iu}/>
-      <PeopleCard name="Miss Someone" desig="Designation" phone="1234567890" photo={iu}/>
+      <PeopleCard animDur={800} name="Miss S. Sneha" desig="President" phone="6381484247" photo={iu} />
+      <PeopleCard animDur={850} name="Miss Someone" desig="Designation" phone="1234567890" photo={iu}/>
+      <PeopleCard animDur={900} name="Miss Someone" desig="Designation" phone="1234567890" photo={iu}/>
+      <PeopleCard animDur={950} name="Miss Someone" desig="Designation" phone="1234567890" photo={iu}/>
+      <PeopleCard animDur={1000} name="Miss Someone" desig="Designation" phone="1234567890" photo={iu}/>
+      <PeopleCard animDur={1050} name="Miss Someone" desig="Designation" phone="1234567890" photo={iu}/>
     </div>
   );
 };
