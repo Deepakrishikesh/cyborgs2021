c7067c5b67e855bb94bf550a7d099a1f1a9ebbd6
Deepak Rishikesh <deepakrishikesh2001@gmail.com>
2021-08-19 19:46:01 +0530

    Date

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -14,7 +14,7 @@ function Landing() {
         </div>
         <div className="flex flex-col justify-center items-center text-center p-2">
           <h1 className="font-bold text-5xl mb-3 drop-shadow">CYBORG's 2021</h1>
-          <p>21 AUG 2021 - ONLINE</p>
+          <p>28 AUG 2021 - ONLINE</p>
         </div>
         <div className="flex flex-col justify-center items-stretch m-2 lg:flex-row">
           <a
