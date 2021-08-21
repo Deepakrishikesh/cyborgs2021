b695f7ae6a4368dd113fb36eef35792029643bbb
Deepak Rishikesh <deepakrishikesh2001@gmail.com>
2021-08-21 07:59:15 +0530

    bug fix

--- src/services/storeId.js
+++ src/services/storeId.js
@@ -2,7 +2,7 @@ import fire from "../firebase";
 
 export const uploadId = async (file) => {
   let d = new Date();
-  let dformat = d.getUTCSeconds()+'_'+d.getUTCMinutes()+'_'+d.getUTCHours()+'_'+d.getUTCMonth()+'_'+d.getUTCHours()+'_';
+  let dformat = d.getUTCSeconds()+'_'+d.getUTCMinutes()+'_'+d.getUTCHours()+'_'+d.getUTCDate()+'_'+d.getUTCMonth()+'_'+d.getUTCFullYear()+'_';
   const fileRef = fire.storage().ref("participants_proof").child(dformat+file.name);
   await fileRef.put(file);
   return await fileRef.getDownloadURL();
